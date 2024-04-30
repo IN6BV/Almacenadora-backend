b35bc700a1b5dac0e4ac54764807d9b5fc89e055
Edwar Chamale <114941106+echamale-2022222@users.noreply.github.com>
2024-04-30 15:37:17 -0600

    Delete

--- Desktop/Almacenadora-backend-feature-taskList/README.md
+++
@@ -1,2 +0,0 @@
-# Almacenadora-backend
-Parte backend del proyecto de almacenadora, elaborado en base a los requerimientos 
--- Desktop/Almacenadora-backend-feature-taskList/src/helpers/db-validators.js
+++
@@ -1,8 +0,0 @@
-import TaskList from "../taskList/taskList.model.js";
-
-export const existeTaskListPorId = async (id) => {
-    const existeTaskList = await TaskList.findById(id);
-    if (!existeTaskList) {
-        throw new Error(`El id ${id} no existe`);
-    }
-}
